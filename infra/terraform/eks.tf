61a5aae65f69cc6ebff471b72d8684ade351c8ca
Charles Apochi <charlesapochi@Charless-MacBook-Pro.local>
2026-04-11 11:41:34 +0100

    infra provisioning with tf

---
+++ infra/terraform/eks.tf
@@ -0,0 +1,18 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  name    = "devops-cluster"
+  kubernetes_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+
+      instance_types = ["t3.medium"]
+    }
+  }
+}
---
+++ infra/terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "charlesapochi"
+    key    = "eks/cluster.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
